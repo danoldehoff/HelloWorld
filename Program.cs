fc9b986375bff0409d916add095b6d4c3b285cd1
danoldehoff <85201914+danoldehoff@users.noreply.github.com>
2021-06-01 17:23:26 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(12.328);
+            Console.WriteLine("Hello World!");
+            Beep();
+        }
+
+        static void Beep()
+        {
+            Console.Beep(15000, 100000);
+        }
+    }
+}
